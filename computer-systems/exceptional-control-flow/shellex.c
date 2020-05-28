3a814c5ba96d6e8e72630a28ecc3d5c86435db5e
wayne.wang <1007677085@qq.com>
2020-05-28 03:46:06 +0000

    fix shellex.c bug

--- computer-systems/exceptional-control-flow/shellex.c
+++ computer-systems/exceptional-control-flow/shellex.c
@@ -1,6 +1,5 @@
 #include "csapp.h"
 #define MAXARGS 128
-#define MAXLINE 128
 
 void eval(char *cmdline);
 int parseline(char *buf, char **argv);
@@ -11,11 +10,10 @@ int main() {
 
   while(1) {
     printf("> ");
-    fgets(cmdline, MAXLINE, stdin);
+    Fgets(cmdline, MAXLINE, stdin);
     if (feof(stdin)) {
       exit(0);
     }
-    printf("cmdline = %s\n", cmdline);
     eval(cmdline);
   }
 }
@@ -23,26 +21,36 @@ int main() {
 void eval(char *cmdline) {
   char *argv[MAXARGS];
   char buf[MAXLINE];
-  int bg;
-  pid_t pid;
+  int bg;                   /* run in background or not */
+  pid_t pid;                /* process id */
   extern char** environ;
 
   strcpy(buf, cmdline);
   bg = parseline(buf, argv);
-  printf("argv[0] = %s\n", argv[0]);
   if (argv[0] == NULL) {
     return;
   }
-
-  if (!builtin_command(argv)) {
-    printf("start fork\n");
-    if ((pid = Fork()) == 0) {
+  int i;
+  for (i = 0; i < MAXARGS; i++) {
+    if (argv[i] == NULL) {
+      break;
+    }
+    /* print argv for debug
+    printf("argv[%d] = %s\n", i, argv[i]);
+    */
+  }
+  if (builtin_command(argv)) {
+    /* print for debug
+    printf("fork new process exec %s\n", argv[0]);
+    */
+    if ((pid = Fork()) == 0) {   /* child runs user job */
       if (execve(argv[0], argv, environ) < 0) {
         printf("%s: Command not found.\n", argv[0]);
         exit(0);
       }
     }
   
+    /* parent waits for foreground job to terminate */
     if (!bg) {
       int status;
       if (waitpid(pid, &status, 0) < 0) {
@@ -55,6 +63,7 @@ void eval(char *cmdline) {
   return;
 }
 
+/* if first arg is a builtin command */
 int builtin_command(char **argv) {
   if (!strcmp(argv[0], "quit")) {
     exit(0);
@@ -62,23 +71,25 @@ int builtin_command(char **argv) {
   if (!strcmp(argv[0], "&")) {
     return 1;
   }
+  if (!strcmp(argv[0], "/usr/bin/ls")) {
+    return 1;
+  }
   return 0;
 }
 
+/* parse the command line and build the argv array */
 int parseline(char *buf, char **argv) {
-  printf("buf = %s", buf);
   char *delim;
   int argc;
   int bg;
 
-  buf[strlen(buf) - 1] == ' ';
+  buf[strlen(buf) - 1] = ' ';
   while (*buf && (*buf == ' ')) {
     buf++;
   }
 
   argc = 0;
   while ((delim = strchr(buf, ' '))) {
-    printf("delim = %s\n", delim)
     argv[argc++] = buf;
     *delim = '\0';
     buf = delim + 1;
