28b1a6b5b072bb6884f0b29feff243a870a49393
wayne.wang <1007677085@qq.com>
2020-06-25 10:37:47 +0000

    practice 10.x

---
+++ computer-systems/system-level-io/pp-10.5.c
@@ -0,0 +1,14 @@
+#include "csapp.h"
+
+int main() {
+  int fd1, fd2;
+  char c;
+
+  fd1 = Open("foo.txt", O_RDONLY, 0);
+  fd2 = Open("foo.txt", O_RDONLY, 0);
+  Read(fd2, &c, 1);
+  Dup2(fd2, fd1);
+  Read(fd1, &c, 1);
+  printf("c = %c\n", c);
+  exit(0);
+}
