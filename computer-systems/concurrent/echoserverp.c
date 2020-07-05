4094fd7cf7757c6012ee255135660d20f272ba9b
wayne.wang <1007677085@qq.com>
2020-07-05 08:42:17 +0000

    practice concurrent programming

--- computer-systems/concurrent/echoserverp.c
+++ computer-systems/concurrent/echoserverp.c
@@ -32,16 +32,3 @@ int main(int argc, char **argv) {
     Close(connfd);
   }
 }
-
-void echo(int connfd) {
-  size_t n;
-  char buf[MAXLINE];
-  rio_t rio;
-
-  Rio_readinitb(&rio, connfd);
-  while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-    printf("server received %d bytes\n", (int) n);
-    Rio_writen(connfd, buf, n);
-  }
-  printf("echo finished\n");
-}
